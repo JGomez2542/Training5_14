facdcb8dc12a1043155a4ac6881d9b5cd62581b1
Mac <mac@MacBook-Pro.local>
2019-05-22 13:57:49 -0400

    Added Coroutine exercises

--- app/src/main/java/com/example/mytrainingapp/activity/ThreadingActivity.kt
+++ app/src/main/java/com/example/mytrainingapp/activity/ThreadingActivity.kt
@@ -12,13 +12,14 @@ import com.example.mytrainingapp.common.MSG_KEY
 import com.example.mytrainingapp.data.entities.Entities
 import com.example.mytrainingapp.data.repository.Repository
 import com.example.mytrainingapp.data.repository.RepositoryImpl
+import com.example.mytrainingapp.managers.TaskManager
 import com.example.mytrainingapp.threads.MyRunnable
 import com.example.mytrainingapp.threads.MyThread
+import com.example.mytrainingapp.utils.snackbar
 import com.example.mytrainingapp.utils.toast
 import kotlinx.android.synthetic.main.activity_threading.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
+import kotlinx.coroutines.*
+import java.lang.Exception
 import kotlin.coroutines.CoroutineContext
 
 class ThreadingActivity : AppCompatActivity(), CoroutineScope, Handler.Callback, View.OnClickListener {
@@ -71,6 +72,77 @@ class ThreadingActivity : AppCompatActivity(), CoroutineScope, Handler.Callback,
         when (v?.id) {
             R.id.btnThread -> MyThread(10).start()
             R.id.btnRunnable -> Thread(MyRunnable(Handler(this), 10)).start()
+            R.id.btnStartSnackbarCoroutine -> {
+                launch {
+                    (1..5).forEach {
+                        Thread.sleep(500)
+                        Log.d(
+                            this::class.java.simpleName,
+                            "Coroutine task $it running on ${Thread.currentThread().name}"
+                        )
+                    }
+                    withContext(Dispatchers.Main) {
+                        snackbar(
+                            message = "Coroutine snackbar running on ${Thread.currentThread().name}", action = "Close",
+                            rootLayout = llMain
+                        )
+                        toast(message = "First for each done")
+                    }
+                    (1..5).forEach {
+                        Thread.sleep(500)
+                        Log.d(
+                            this::class.java.simpleName,
+                            "Coroutine task $it running on ${Thread.currentThread().name}"
+                        )
+                    }
+                    withContext(Dispatchers.Main) {
+                        snackbar(
+                            message = "Coroutine snackbar running on ${Thread.currentThread().name}", action = "Close",
+                            rootLayout = llMain
+                        )
+                        toast(message = "Second for each done")
+                    }
+                    (1..5).forEach {
+                        Thread.sleep(500)
+                        Log.d(
+                            this::class.java.simpleName,
+                            "Coroutine task $it running on ${Thread.currentThread().name}"
+                        )
+                    }
+                    withContext(Dispatchers.Main) {
+                        toast(message = "Done!")
+                    }
+                }
+            }
+            R.id.btnStartDelayedCoroutine -> {
+                launch(Dispatchers.Main) {
+                    snackbar(
+                        message = "Coroutine snackbar running on ${Thread.currentThread().name}",
+                        action = "Close", rootLayout = llMain
+                    )
+                    delay(6000)
+                    tvResult.text = "First update"
+                    delay(2000)
+                    tvResult.text = "Second update"
+                    delay(2000)
+                    toast(message = "Done!")
+                }
+            }
+
+            //First implement without using isActive
+            R.id.btnStartGlobalCoroutine -> {
+                GlobalScope.launch {
+                    (1..20).forEach {
+                        if (isActive) {
+                            Thread.sleep(1000)
+                            Log.d(
+                                this::class.java.simpleName,
+                                "Coroutine task $it running on ${Thread.currentThread().name}"
+                            )
+                        }
+                    }
+                }
+            }
         }
     }
 
